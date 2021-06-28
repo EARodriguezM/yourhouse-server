5e0c2f2028fa1f19fc5d8ba4bc447aeb01f55eda
EARodriguezM <esarodriguezme@gmail.com>
2021-06-28 18:10:37 -0500

    add cors with callback

--- src/index.ts
+++ src/index.ts
@@ -4,16 +4,36 @@ import { ApolloServer } from 'apollo-server';
 import { connectDb } from './database';
 import { typeDefs, resolvers } from './graphql';
 
+
+const corsOptions = {
+  origin: 'https://yourhouses.netlify.app',
+  credentials: true
+}
+
 const start = async () => {
   const db = await connectDb();
   const server = new ApolloServer({
-    cors: false,
+    cors: {
+      credentials: true,
+      origin: (origin, callback) => {
+          const whitelist = [
+              "https://yourhouses.netlify.app"
+          ];
+
+          if (whitelist.indexOf(origin!) !== -1) {
+              callback(null, true)
+          } else {
+              callback(new Error("Not allowed by CORS"))
+          }
+      }
+  },
     resolvers,
     typeDefs,
     context: ({ req, res }) => ({ db, req, res }),
     playground: true,
     introspection: true,
   });
+
   server
     .listen({ port: process.env.PORT || 5000 })
     .then(({ url }) => console.log(`Server ready at ${url}. `));
